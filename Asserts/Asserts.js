eb2f71dacc9e07c6761292622814ffa59096f672
Sehar Ajmal <seharajmal452@gmail.com>
2025-12-17 16:15:30 +0500

    Header Component
    Header component completion

---
+++ Asserts/Asserts.js
@@ -0,0 +1,17 @@
+
+import logo from './logo.png'
+import arrows from './arrow.png'
+
+export const asserts = {
+    logo,
+    arrows
+}
+function Asserts() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Asserts
---
+++ Components/Header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { asserts } from "@/Asserts/Asserts";
+import Image from "next/image";
+
+function Header() {
+  return (
+    <div className="py-5 px-5 md:px-12 lg:px-28">
+      <div className="flex justify-between items-center">
+        {/* Smaller Logo */}
+        <Image src={asserts.logo} width={180}height={40} alt="Logo" className="w-32.5 sm:w-auto"
+        />
+
+        {/* Button with softer shadow */}
+        <button
+          className="flex items-center gap-2 font-medium py-2 px-4 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000]"
+        >
+          Get Started
+          <Image src={asserts.arrows} width={20} height={20} alt="Arrow" className="w-5"
+          />
+        </button>
+      </div>
+      <div className='text-center my-8'>
+        <h1 className='text-3xl sm:text-5xl font-medium'>Latest Blogs</h1>
+        <p className="mt-10 max-w-185 m-auto text-xs sm:text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur aperiam laboriosam iusto doloremque aliquam sit laborum dolores enim officiis?</p>
+
+        <form action="" className="flex justify-between max-w-125 scale-75 sm:scale-100 mx-auto mt-10 border border-black  shadow-[-7px_7px_0px_#000]">
+          <input type="email" placeholder="Enter your Email." className="pl-4 outline-none"/>
+          <button type='submit' className="border border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white">Subcribe</button>
+        </form>
+      </div>
+
+
+
+
+
+    </div>
+  );
+}
+
+export default Header;
